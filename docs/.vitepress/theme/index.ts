9c5ba3cf6454f585c1b0d90dd96e76653ccf0698
TooManyDe <dedebuxiu@gmail.com>
2026-06-21 12:39:28 +0800

    更新 index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -16,11 +16,26 @@ export default {
 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
 			"doc-after": () => h(Comment),
 			"doc-top": () => h(ImageViewer),
+			"aside-top": () => h(GoBack),
 		});
 	},
 
 	enhanceApp({ app, router }: any) {
 		app.component("Comment", Comment);
 		app.component("ImageViewer", ImageViewer);
+		app.component("GoBack", GoBack);
+
+		router.onAfterRouteChanged = (to: string) => {
+			// 兼容旧博客的中文路径，重定向到新路径，避免外链失效
+			if (to.startsWith(encodeURI('/博客/'))) {
+				const newUrl = to.replace(encodeURI('/博客/'), '/posts/')
+				window.location.href = newUrl
+			}
+
+			if (to.startsWith(encodeURI('/笔记/'))) {
+				const newUrl = to.replace(encodeURI('/笔记/'), '/notes/')
+				window.location.href = newUrl
+			}
+		}
 	},
 };
