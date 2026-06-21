52e3ccc23693888053b150cd0768fbaead46883d
TooManyDe <dedebuxiu@gmail.com>
2026-06-21 12:42:44 +0800

    更新 index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -21,6 +21,18 @@ export default {
 	enhanceApp({ app, router }: any) {
 		app.component("Comment", Comment);
 		app.component("ImageViewer", ImageViewer);
+
+		router.onAfterRouteChanged = (to: string) => {
+			// 兼容旧博客的中文路径，重定向到新路径，避免外链失效
+			if (to.startsWith(encodeURI('/博客/'))) {
+				const newUrl = to.replace(encodeURI('/博客/'), '/posts/')
+				window.location.href = newUrl
+			}
+
+			if (to.startsWith(encodeURI('/笔记/'))) {
+				const newUrl = to.replace(encodeURI('/笔记/'), '/notes/')
+				window.location.href = newUrl
+			}
 		}
 	},
 };
