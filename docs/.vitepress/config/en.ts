2240ec9fcf4d34598064a66a152369c229074594
TooManyDe <dedebuxiu@gmail.com>
2026-06-22 04:23:11 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -5,7 +5,8 @@ const RSS_ICON_SVG = '<svg xmlns="http://www.w3.org/2000/svg" width="512" height
 
 // 导航配置
 const navConfig: DefaultTheme.NavItem[] = [
-  { text: "Archive", link: "/en/archive/", activeMatch: '/en/archive/' },
+  { text: "Archive", link: "/en/archive", activeMatch: '/en/archive' },
+  { text: "Friends", link: "/friends", activeMatch: '/friends' },
 ]
 
 // 社交链接配置
